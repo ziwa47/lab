cf77f7dd5a13e4f5718d1f3ea0b0779846317788
杜 阿輝 <GMziwa47@hotmail.com>
2019-03-15 09:50:26 +0800

    Where Func<T1,TResult>

--- CSharpAdvanceDesignTests/JoeyWhereTests.cs
+++ CSharpAdvanceDesignTests/JoeyWhereTests.cs
@@ -1,7 +1,9 @@
-﻿using Lab.Entities;
+﻿using System;
+using Lab.Entities;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System.Collections.Generic;
+using ExpectedObjects;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -23,7 +25,8 @@ namespace CSharpAdvanceDesignTests
                 new Product {Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo"}
             };
 
-            //var actual = JoeyWhere(products);
+            var actual = JoeyWhere(products,
+                p => p.Price > 200 && p.Price < 500);
 
             var expected = new List<Product>
             {
@@ -32,7 +35,58 @@ namespace CSharpAdvanceDesignTests
                 new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"}
             };
 
-            //expected.ToExpectedObject().ShouldMatch(actual);
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [Test]
+        public void find_products_that_price_between_200_and_500_and_cost_more_than_30()
+        {
+            var products = new List<Product>
+            {
+                new Product {Id = 1, Cost = 11, Price = 110, Supplier = "Odd-e"},
+                new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
+                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"},
+                new Product {Id = 5, Cost = 51, Price = 510, Supplier = "Momo"},
+                new Product {Id = 6, Cost = 61, Price = 610, Supplier = "Momo"},
+                new Product {Id = 7, Cost = 71, Price = 710, Supplier = "Yahoo"},
+                new Product {Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo"}
+            };
+
+            //var actual = JoeyWhereFilterCost(products);
+            var actual = JoeyWhere(products, p => p.Price > 200 && p.Price < 500 & p.Cost > 30);
+
+            var expected = new List<Product>
+            {
+                //new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
+                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"}
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        //private List<Product> JoeyWhereFilterCost(List<Product> products)
+        //{
+        //    var result = new List<Product>();
+        //    foreach (var product in products)
+        //    {
+        //        if (product.Price > 200 & product.Price < 500 && product.Cost > 30)
+        //            result.Add(product);
+        //    }
+
+        //    return result;
+        //}
+
+        private List<Product> JoeyWhere(List<Product> products, Func<Product, bool> predicate)
+        {
+            var result = new List<Product>();
+            foreach (var p in products)
+            {
+                if (predicate(p))
+                    result.Add(p);
+            }
+            return result;
         }
     }
 }
