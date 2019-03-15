b6145d62e30a5816a4c5de333e43a9c894fefc73
杜 阿輝 <GMziwa47@hotmail.com>
2019-03-15 13:13:33 +0800

    new spec

--- CSharpAdvanceDesignTests/JoeySelectTests.cs
+++ CSharpAdvanceDesignTests/JoeySelectTests.cs
@@ -16,7 +16,7 @@ namespace CSharpAdvanceDesignTests
         {
             var urls = GetUrls();
 
-            var actual = JoeySelect(urls, 
+            var actual = JoeySelect(urls,
                 url => url.Replace("http://", "https://"));
             var expected = new List<string>
             {
@@ -35,7 +35,7 @@ namespace CSharpAdvanceDesignTests
             var urls = GetUrls();
 
             var actual = JoeySelect(urls,
-                url=>url.Replace("http://", "https://")+"/joey");
+                url => url.Replace("http://", "https://") + "/joey");
             var expected = new List<string>
             {
                 "https://tw.yahoo.com/joey",
@@ -47,20 +47,118 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
-        private IEnumerable<string> JoeySelect(IEnumerable<string> urls, Func<string,string> mapper)
+        [Test]
+        public void get_full_name()
         {
-            var result = new List<string>();
-            foreach (var url in urls)
+            var employees = new List<Employee>
             {
-                var mapResult = mapper(url);
-                result.Add(mapResult);
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "David", LastName = "Chen"}
+            };
+            var expected = new[]
+            {
+                "Joey-Chen",
+                "Tom-Li",
+                "David-Chen",
+            };
+
+            var actual = JoeySelect(employees, e => $"{e.FirstName}-{e.LastName}");
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [Test]
+        public void get_full_length()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "David", LastName = "Chen"}
+            };
+            var expected = new[]
+            {
+                8,5,9
+            };
+
+            var actual = JoeySelect(employees, e => $"{e.FirstName}{e.LastName}".Length);
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [Test]
+        public void get_full_name_with_no()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "David", LastName = "Chen"}
+            };
+            var expected = new[]
+            {
+                "1.Joey-Chen",
+                "2.Tom-Li",
+                "3.David-Chen",
+            };
+
+            var actual = JoeySelect(employees, (e, i) => $"{i}.{e.FirstName}-{e.LastName}");
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+
+
+        //private IEnumerable<string> JoeySelect(IEnumerable<string> urls, Func<string, string> selector)
+        //{
+        //    var result = new List<string>();
+        //    foreach (var url in urls)
+        //    {
+        //        var mapResult = selector(url);
+        //        result.Add(mapResult);
+        //    }
+
+        //    return result;
+        //}
+
+        //private IEnumerable<string> JoeySelect(IEnumerable<Employee> emps, Func<Employee, string> selector)
+        //{
+        //    var result = new List<string>();
+        //    foreach (var emp in emps)
+        //    {
+        //        var mapResult = selector(emp);
+        //        result.Add(mapResult);
+        //    }
+
+        //    return result;
+        //}
+
+        private IEnumerable<TResult> JoeySelect<TSource, TResult>(IEnumerable<TSource> sources,
+            Func<TSource, TResult> selector)
+        {
+            var result = new List<TResult>();
+            foreach (var source in sources)
+            {
+                result.Add(selector(source));
             }
 
             return result;
         }
 
+        private IEnumerable<TResult> JoeySelect<TSource, TResult>(IEnumerable<TSource> sources,
+            Func<TSource, int, TResult> selector)
+        {
+            //var result = new List<TResult>();
+            var index = 0;
+            foreach (var source in sources)
+            {
+                //result.Add(selector(source, ++index));
+                yield return selector(source, ++index);
+            }
 
-        //private IEnumerable<string> JoeySelectAppend(IEnumerable<string> urls, Func<string,string> mapper)
+            //return result;
+        }
+
+
+        //private IEnumerable<string> JoeySelectAppend(IEnumerable<string> urls, Func<string,string> selector)
         //{
         //    var result = new List<string>();
         //    foreach (var url in urls)
@@ -71,7 +169,7 @@ namespace CSharpAdvanceDesignTests
 
         //    return result;
         //}
-      
+
 
         private static IEnumerable<string> GetUrls()
         {
