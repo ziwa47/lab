976efa6b8a3c3fafc9372dac8226828490ca3ce4
杜 阿輝 <GMziwa47@hotmail.com>
2019-03-17 21:50:55 +0800

    groupby

--- CSharpAdvanceDesignTests/JoeyGroupByTests.cs
+++ CSharpAdvanceDesignTests/JoeyGroupByTests.cs
@@ -5,9 +5,9 @@ using Lab.Entities;
 using NUnit.Framework;
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Lab;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -47,55 +47,5 @@ namespace CSharpAdvanceDesignTests
             }
             return lookup;
         }
-    }
-
-    internal class MyLookUp : IEnumerable<IGrouping<string, Employee>>
-    {
-        private Dictionary<string, List<Employee>> _lookup = new Dictionary<string, List<Employee>>();
-
-        public IEnumerator<IGrouping<string, Employee>> GetEnumerator()
-        {
-            return _lookup.Select(r => new MyGrouping(r.Key, r.Value)).GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public void AddElement(string groupKeySelector, Employee employee)
-        {
-            if (_lookup.ContainsKey(groupKeySelector))
-            {
-                _lookup[groupKeySelector].Add(employee);
-            }
-            else
-            {
-                _lookup.Add(groupKeySelector, new List<Employee>() { employee });
-            }
-        }
-    }
-
-    internal class MyGrouping : IGrouping<string, Employee>
-    {
-        private readonly IEnumerable<Employee> _collection;
-
-        public MyGrouping(string key, IEnumerable<Employee> collection)
-        {
-            Key = key;
-            _collection = collection;
-        }
-
-        public IEnumerator<Employee> GetEnumerator()
-        {
-            return _collection.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public string Key { get; }
     }
 }
---
+++ Lab/MyLookUp.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Lab.Entities;
+
+namespace Lab
+{
+    public class MyGrouping : IGrouping<string, Employee>
+    {
+        private readonly IEnumerable<Employee> _collection;
+
+        public MyGrouping(string key, IEnumerable<Employee> collection)
+        {
+            Key = key;
+            _collection = collection;
+        }
+
+        public IEnumerator<Employee> GetEnumerator()
+        {
+            return _collection.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public string Key { get; }
+    }
+
+    public class MyLookUp : IEnumerable<IGrouping<string, Employee>>
+    {
+        private Dictionary<string, List<Employee>> _lookup = new Dictionary<string, List<Employee>>();
+
+        public IEnumerator<IGrouping<string, Employee>> GetEnumerator()
+        {
+            return _lookup.Select(r => new MyGrouping(r.Key, r.Value)).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void AddElement(string groupKeySelector, Employee employee)
+        {
+            if (_lookup.ContainsKey(groupKeySelector))
+            {
+                _lookup[groupKeySelector].Add(employee);
+            }
+            else
+            {
+                _lookup.Add(groupKeySelector, new List<Employee>() { employee });
+            }
+        }
+    }
+}
