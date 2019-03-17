bf45c3c84bf8debee436d187e33c84cce0712e99
杜 阿輝 <GMziwa47@hotmail.com>
2019-03-17 15:19:58 +0800

    aggregate

--- CSharpAdvanceDesignTests/JoeyAggregateTests.cs
+++ CSharpAdvanceDesignTests/JoeyAggregateTests.cs
@@ -1,9 +1,10 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 
 namespace CSharpAdvanceDesignTests
 {
-    [Ignore("not yet")]
     [TestFixture]
     public class JoeyAggregateTests
     {
@@ -17,16 +18,36 @@ namespace CSharpAdvanceDesignTests
                 30, 80, 20, 40, 25
             };
 
-            var actual = JoeyAggregate(drawlingList, balance);
+            var actual = JoeyAggregate(drawlingList, balance, 
+                (draw, myBalance) => draw > myBalance ? myBalance : myBalance - draw, 
+                myBalance1 => myBalance1.ToString("0.00"));
 
-            var expected = 10.91m;
+            //var expected = 10.91m;
+            var expected = "10.91";
 
             Assert.AreEqual(expected, actual);
         }
 
-        private decimal JoeyAggregate(IEnumerable<int> drawlingList, decimal balance)
+        private string JoeyAggregate(IEnumerable<int> drawlingList, decimal balance, Func<int, decimal, decimal> func, Func<decimal, string> resultSelector)
         {
-            throw new System.NotImplementedException();
+            var drawEnumerator = drawlingList.GetEnumerator();
+            var myBalance = balance;
+            while (drawEnumerator.MoveNext())
+            {
+                var draw = drawEnumerator.Current;
+                myBalance = func(draw, myBalance);
+            }
+
+            //foreach (var draw in drawlingList)
+            //{
+            //    myBalance = draw > myBalance ? myBalance : myBalance - draw;
+            //}
+
+            return resultSelector(myBalance);
+
+            //return myBalance;
+
+            
         }
     }
 }
